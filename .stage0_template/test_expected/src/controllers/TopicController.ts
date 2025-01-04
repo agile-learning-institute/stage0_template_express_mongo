3056afbea0c89c358a8feaef95ca90641f4f2f65
Mike Storey <mike@agile-learning.institute>
2025-01-03 22:15:29 -0500

    Sink Controller first pass refactor

--- .stage0_template/test_expected/src/controllers/TopicController.ts
+++ .stage0_template/test_expected/src/controllers/TopicController.ts
@@ -7,17 +7,34 @@ export default class TopicController {
   constructor() {
   }
 
-  public getTopic = async (req: Request, res: Response) => {
+  public getTopics = async (req: Request, res: Response) => {
     try {
       const token = decodeToken(req);
       const breadcrumb = createBreadcrumb(token, req);
-      const results = await TopicService.FindTopic(req.query, token);
-      res.json(results);
+      const topics = await TopicService.GetTopics(req.query, token);
       res.status(200);
+      res.json(topics);
       console.info("GetTopic Completed", JSON.stringify(breadcrumb));
     } catch (error) {
+      const message = this.getMessage(error);
       res.status(500);
-      console.error("GetTopic Failed", error);
+      res.json({error:message});
+      console.warn("GetTopics Failed:", message);
     }
-  }
+
+  public getTopic = async (req: Request, res: Response) => {
+    try {
+      const theId = req.params.topicId;
+      const token = decodeToken(req);
+      const breadcrumb = createBreadcrumb(token, req);
+      const topic = await TopicService.GetTopic(theId, token);
+      res.status(200);
+      res.json(topic);
+      console.info("GetTopic %s Completed with %s", theId, JSON.stringify(breadcrumb));
+    } catch (error) {
+      const message = this.getMessage(error);
+      res.status(500);
+      res.json({error:message});
+      console.warn("GetTopic Failed:", message);
+    }
 }
